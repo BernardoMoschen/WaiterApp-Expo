5d617e1f932e1507c0ead8ff9e944ff68647e7b6
BernardoMoschen <bernardomoschen.dev@gmail.com>
2024-07-09 15:10:49 -0300

    wip: main components

--- src/Main/Main.tsx
+++ src/Main/Main.tsx
@@ -1,12 +1,22 @@
 import React from "react";
 import { Text } from "../components/Text";
-import { Container } from "./styles";
+import {
+    CategoriesContainer,
+    Container,
+    Footer,
+    MenuContainer,
+} from "./styles";
 import { Header } from "../components/Header/Header";
 
 export const Main = () => {
     return (
-        <Container>
-            <Header />
-        </Container>
+        <>
+            <Container>
+                <Header />
+                <CategoriesContainer />
+                <MenuContainer />
+            </Container>
+            <Footer />
+        </>
     );
 };
