c9469e69abe143d4623e5fb1ff11f4ebc52d8a72
BernardoMoschen <bernardomoschen.dev@gmail.com>
2024-07-09 15:01:04 -0300

    wip: container and statusBar

---
+++ src/Main/Main.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { Text } from "../components/Text";
+import { Container } from "./styles";
+
+export const Main = () => {
+    return (
+        <Container>
+            <Text>Main rendered</Text>
+        </Container>
+    );
+};
