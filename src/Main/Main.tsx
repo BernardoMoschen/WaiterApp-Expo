ce1f0bb2510a46521cf611dd3d9d22e60b9be1bc
BernardoMoschen <bernardomoschen.dev@gmail.com>
2024-07-09 15:05:04 -0300

    feat: header

--- src/Main/Main.tsx
+++ src/Main/Main.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 import { Text } from "../components/Text";
 import { Container } from "./styles";
+import { Header } from "../components/Header/Header";
 
 export const Main = () => {
     return (
         <Container>
-            <Text>Main rendered</Text>
+            <Header />
         </Container>
     );
 };
