199b390f5acd6612ab4e20a36fc60323ce836107
BernardoMoschen <bernardomoschen.dev@gmail.com>
2024-07-17 15:32:04 -0300

    chore: rm unnecessary imports

--- src/Main/Main.tsx
+++ src/Main/Main.tsx
@@ -1,18 +1,14 @@
 import React from "react";
 import { Container, Footer, FooterContainer } from "./styles";
 import { Logo } from "../components/Logo/Logo";
-import ExpoStatusBar from "expo-status-bar/build/ExpoStatusBar";
 import { Home } from "../pages/My/pages/Home/Home";
 
 export const Main = () => {
     return (
         <>
-            <ExpoStatusBar />
             <Container>
-                <>
-                    <Logo />
-                    <Home />
-                </>
+                <Logo />
+                <Home />
             </Container>
             <Footer>
                 <FooterContainer></FooterContainer>
