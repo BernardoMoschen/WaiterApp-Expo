b954f40c198f535b5ba97882d03108579909838c
BernardoMoschen <bernardomoschen.dev@gmail.com>
2024-07-17 17:27:18 -0300

    wip:forgotPassword page

---
+++ src/pages/SignIn/ForgotPassword/ForgotPassword.tsx
@@ -0,0 +1,11 @@
+// import { Wizard } from 'react-use-wizard'
+import { SendEmail, CheckEmail } from "./steps";
+
+export const ForgotPassword = () => (
+    <>
+        {/* <Wizard> */}
+        {/* <SendEmail /> */}
+        <CheckEmail />
+        {/* </Wizard> */}
+    </>
+);
